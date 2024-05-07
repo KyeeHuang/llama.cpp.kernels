7d9431df1c47f95fd7b33e1f7ab97167897c3b54
KyeeHuang <45361174+KyeeHuang@users.noreply.github.com>
2024-05-07 23:43:11 +0800

    update softmax kernel

---
+++ kernels/soft_max_f32.cu
@@ -0,0 +1,30 @@
+static __global__ void soft_max_f32(const float* x, float* dst, const int ncols) {
+  const int row = blockDim.y * blockIdx.y + threadIdx.y;
+  const int block_size = blockDim.x;
+  const int tid = threadIdx.x;
+  float tmp = 0.0;
+  for (int block_start = 0; block_start < ncols; block_start += block_size) {
+    const int col = block_start + tid;
+    if (col >= ncols) {
+      break;
+    }
+    const int i = row * ncols + col;
+    const float val = expf(x[i]);
+    tmp += val;
+    dst[i] = val;
+  }
+
+#pragma unroll
+  for (int mask = 16; mask > 0; mask >>= 1) {
+    tmp += __shfl_xor_sync(0xffffffff, tmp, mask, 32);
+  }
+
+  for (int block_start = 0; block_start < ncols; block_start += block_size) {
+    const int col = block_start + tid;
+    if (col >= ncols) {
+      break;
+    }
+    const int i = row * ncols + col;
+    dst[i] /= tmp;
+  }
+}
